8aaa1de287d4567e4606f96e21cf96c3f41f8524
Arindam Shukla <arindamshrish@gmail.com>
2025-03-05 14:18:58 +0530

    completed the script with custome text and number of iterations

--- auto_typing.py
+++ auto_typing.py
@@ -1,14 +1,24 @@
 import pyautogui
 import time
 
+# to take inputs for the script
+print("How many times do the text need to be send: ")
+itter=int(input())
+
+print("\nwhat is the text needed to be send: ")
+word=str(input())
+
+
 # Wait for a few seconds to allow switching to the desired input field
 time.sleep(5)
 
 # The word to type
-word = "Hello"
+# word = "Hello"
 
+# looping to get desired no of outputs
+for i in range(0,itter): 
 # Type the word
-pyautogui.write(word)
+    pyautogui.write(word)
 
 # Press Enter
-pyautogui.press("enter")
+    pyautogui.press("enter")
